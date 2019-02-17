99aebc6ccda7b5c5526fcede2d7f21f69588fcb8
JustinBeaudry <beaudry.justin@gmail.com>
2019-02-17 15:19:46 -0800

    moves index.d.ts to base.d.ts

---
+++ base.d.ts
@@ -0,0 +1,8 @@
+// Type definitions for characterSets 1.x.x
+// Project: https://github.com/JustinBeaudry/character-sets
+// Definitions by: Justin Beaudry <https://github.com/JustinBeaudry>
+// Definitions: https://github.com/JustinBeaudry/character-sets
+
+export function toArray(): string[];
+export function normalize(): string[];
+export function includes(charset: string): boolean;
