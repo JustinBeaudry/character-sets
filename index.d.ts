0b769ebd0c3c01e62b08db0ee4062dd871274d2d
JustinBeaudry <beaudry.justin@gmail.com>
2019-02-17 06:20:59 -0800

    adds TypeScript definition file

---
+++ index.d.ts
@@ -0,0 +1,10 @@
+// Type definitions for characterSets 1.0.0
+// Project: https://github.com/JustinBeaudry/character-sets
+// Definitions by: Justin Beaudry <https://github.com/JustinBeaudry>
+// Definitions: https://github.com/JustinBeaudry/character-sets
+
+declare namespace charactersSets {
+    function toArray(): string[];
+    function normalize(): string[];
+    function includes(charset: string): boolean;
+}
